9a16eecfd6c2a7432d281f71278ce1324d540e62
Stéphane <stephane.escandell@gmail.com>
2014-12-24 10:47:22 +0100

    Xamarin - Phone

---
+++ Xamarin/GalleryProject/GalleryProject.Core/Model/Picture.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GalleryProject.Core.Model
+{
+    public class Picture
+    {
+        public string Name { get; set; }
+        public string SourceUrl { get; set; }
+    }
+}
--- Xamarin/GalleryProject/GalleryProject.Core/ViewModels/FirstViewModel.cs
+++ Xamarin/GalleryProject/GalleryProject.Core/ViewModels/FirstViewModel.cs
@@ -1,24 +1,23 @@
 using System.Collections.Generic;
 using Cirrious.MvvmCross.ViewModels;
+using GalleryProject.Core.Model;
 
 namespace GalleryProject.Core.ViewModels
 {
-    public class FirstViewModel 
-		: MvxViewModel
+    public class FirstViewModel
+        : MvxViewModel
     {
-		private List<string> _items = null;
-        public List<string> Items
-		{
-            get
-            {
-                if (null != _items) return _items;
+        public FirstViewModel()
+        {
+            Items = new List<Picture>();
+            for (var i = 0; i < 200; i++)
+                Items.Add(new Picture()
+                {
+                    Name = i.ToString(),
+                    SourceUrl = "http://lorempixel.com/1024/768/abstract/?v=" + i.ToString()
+                });
+        }
 
-                _items = new List<string>();
-                for (var i=0; i<200; i++)
-                    _items.Add("http://lorempixel.com/1024/768/abstract/?v=" + i.ToString());
-
-                return _items;
-            }
-		}
+        public List<Picture> Items { get; private set; }
     }
 }
