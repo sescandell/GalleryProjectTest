d8279ad913a21a2c39bdd48b8ad38d6be8b5236f
Stéphane <stephane.escandell@gmail.com>
2014-12-29 09:33:21 +0100

    Apply patch from @tittof in https://github.com/MvvmCross/MvvmCross/pull/851

--- Xamarin/GalleryProject/GalleryProject.Droid/Setup.cs
+++ Xamarin/GalleryProject/GalleryProject.Droid/Setup.cs
@@ -1,7 +1,12 @@
 using Android.Content;
+using Android.Graphics;
+using Cirrious.CrossCore;
 using Cirrious.CrossCore.Platform;
 using Cirrious.MvvmCross.Droid.Platform;
+using Cirrious.MvvmCross.Plugins.DownloadCache;
+using Cirrious.MvvmCross.Plugins.DownloadCache.Droid;
 using Cirrious.MvvmCross.ViewModels;
+using GalleryProject.Droid.DownloadCache;
 
 namespace GalleryProject.Droid
 {
@@ -13,7 +18,20 @@ namespace GalleryProject.Droid
 
         protected override IMvxApplication CreateApp()
         {
+            // Override DownloadCache Singleton
+            Mvx.RegisterSingleton<IMvxImageCache<Bitmap>>(() => CreateCache());
             return new Core.App();
+        }
+
+        private BsImageCache<Bitmap> CreateCache()
+        {
+            var configuration = MvxDownloadCacheConfiguration.Default;
+
+            var fileDownloadCache = new MvxFileDownloadCache(configuration.CacheName,
+                                                             configuration.CacheFolderPath,
+                                                             configuration.MaxFiles,
+                                                             configuration.MaxFileAge);
+            return new BsImageCache<Bitmap>(fileDownloadCache, configuration.MaxInMemoryFiles, configuration.MaxInMemoryBytes);
         }
 		
         protected override IMvxTrace CreateDebugTrace()
